a31b18770854cc0d045838bfbd3b2a9b35f7f492
Eric Juo <camphor0614@gmail.com>
2021-08-02 23:45:51 +0800

    Add answer of consensus and profile, and update config.py and utils.py

--- rosalind_tools/config.py
+++ rosalind_tools/config.py
@@ -1,3 +1,6 @@
 from pathlib import Path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 data_dir = Path('../data/raw')
---
+++ rosalind_tools/utils.py
@@ -0,0 +1,61 @@
+from collections import Counter
+# Create a class for sequence record so that we can quickly assesse sequence ID and sequence
+class Record:
+    def __init__(self, record):
+        self.id = record['name']
+        self.seq = record['seq']
+
+def parse_fasta(f):
+    # Create an empty list for storing records
+    records = []
+    # Set an default empty record
+    record = {
+        'name': '',
+        'seq': ''
+    }
+    # Initiate while loop
+    line = True
+    # Set default first line is not the end of record
+    end_of_record = False
+    while line:
+        # Read line by line as while loop runs, and also strip off /n at the end of each line
+        line = f.readline().strip()
+        if not line: # If line is empty, that is not False, skip this loop
+            continue
+
+        if line.startswith('>'):
+            # If we find the '>' again, that is the end of previous record, so we append the record, and then empty the record
+            if end_of_record:
+                r = Record(record)
+                records.append(r)
+                record = {
+                    'name': '',
+                    'seq': ''
+                }
+            # Set the end_of_record as True after we pass the first record name
+            end_of_record = True
+            record['name'] = line.strip('>')
+
+        else:
+            # line not startswith '>' are all sequences
+            record['seq'] += line
+    # Append the last record after the end of while loop
+    r = Record(record)
+    records.append(r)
+    return records
+
+def gc(record):
+    # Retrun GC content of record seqeunce, the number is expressed as % and rounded up to 6th decimal number
+    nb_counts = Counter(record.seq.upper())
+    return round((nb_counts['C'] + nb_counts['G']) / len(record.seq) * 100, 6)
+
+def max_gc(records):
+    # Guess the first record has the largest GC content
+    max_record = records[0]
+    # Compare GC contents of records one by one
+    for record in records:
+        if gc(record) > gc(max_record):
+            max_record = record
+    print(max_record.id)
+    print(gc(max_record))
+    return max_record
